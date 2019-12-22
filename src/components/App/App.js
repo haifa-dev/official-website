3bcd2431ac89f9181a3bb1dae61eb41ada28d4ec
David Klein <David@kubiyax.com>
2019-12-22 14:01:25 +0200

    Applied changes from Dnku's update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,57 +1,33 @@
 import React from 'react';
-import logo from './../../logo.svg';
-import { useParams } from 'react-router';
+
+//import './App.css';
 import { Route, Switch, BrowserRouter } from 'react-router-dom'
-import './App.css';
 
-import LandingPage from '../pages/LandingPage/LandingPage';
+import NavBar from  '../NavBar/NavBar'
+import Home from '../Pages/Home/Home'
+import Project from '../Pages/Project/Project'
+import Footer from '../Footer/Footer'
+import NotFoundPage  from '../Pages/NotFoundPage/NotFoundPage'
 
-const Home = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-        </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-        </a>
-    </header>
-  </div>
-);
 
-const NotFoundPage = () => <h1>NOT FOUND</h1>
-
-const Project = () => {
-  const { id } = useParams();
-
-  return (
-    <div>
-      <h3>ID: {id}</h3>
-    </div>
-  );
-}
-
-const ProjectRequest = () => <h1>Project Requests</h1>
-
-const Routes = () => (
+const Routes = () => ( 
   <Switch>
-    <Route exact path="/" component={LandingPage} />
-    <Route exact path="/about" component={() => <h1>About</h1>} />
-    <Route exact path="/portfolio/projects/:id" component={Project} />
-    <Route exact path="/project-request" component={ProjectRequest} />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/projects/:id" component={Project} />
+    <Route exact path="/project" component={Project} />
     <Route exact path="*" component={NotFoundPage} />
   </Switch>
 )
 
 const App = () => (
   <BrowserRouter>
+    <div className="fixed-top bg-secondary">
+      <div className="container">
+        <NavBar></NavBar>
+      </div>
+    </div>
     <Routes />
+    <Footer /> 
   </BrowserRouter>
 )
 
---
+++ src/components/RequestsForm/RequestsForm.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const RequestsForm = () => {
+  
+    return (
+      <div>
+        <h3>RequestsForm</h3>
+      </div>
+    );
+  }
+    
+export default  RequestsForm; 
+
