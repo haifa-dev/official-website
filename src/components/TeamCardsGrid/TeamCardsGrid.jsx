ca104fac3705244609c634f82fb1c106b2652800
PolarBearOC <polarbear.oc@gmail.com>
2020-01-15 20:54:32 +0200

    Separated members service from team card grid component

--- src/components/TeamCardsGrid/TeamCardsGrid.jsx
+++ src/components/TeamCardsGrid/TeamCardsGrid.jsx
@@ -1,7 +1,7 @@
 import React, { useReducer, useState, useEffect, useRef } from 'react'
-
+import { getMembersAsync } from '../../services/members.service';
 import SingleCard from './SingleCard';
-import styles from './TeamCardsGrid.module.scss'
+import styles from './TeamCardsGrid.module.scss';
 
 
 /**
@@ -14,38 +14,15 @@ const CardsGrid = () => {
 
     // #region members
 
-    const [membersList, setMembersList] = useState([{
-        id: 1,
-        name: "member 1",
-        about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-        img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-        githubLink: "https://github.com/explore",
-        linkedinLink: "https://www.linkedin.com/"
-    },
-    {
-        id: 2,
-        name: "member 2",
-        about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-        img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-        githubLink: "https://github.com/explore",
-        linkedinLink: "https://www.linkedin.com/"
-    },
-    {
-        id: 3,
-        name: "member 3",
-        about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-        img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-        githubLink: "https://github.com/explore",
-        linkedinLink: "https://www.linkedin.com/"
-    }])
+    const [membersList, setMembersList] = useState([])
 
     // #endregion
 
     const teamCardsRef = useRef(null);
     
-    const [perLoad] = useState(3);
-    const [total, setTotal] = useState(0);
-    const [isFetching, setIsFetching] = useState(false);
+    const [perPage] = useState(3);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [isFetching, setIsFetching] = useState(true);
 
 
     const handleScroll = () => {
@@ -59,182 +36,24 @@ const CardsGrid = () => {
 
     const loadMore = async () => {
         // TODO: remove timeout on fetching real data
-        setTimeout(() => {
-            // temporary members data
-            const members = [
-
-                {
-                    id: 4,
-                    name: "member 4",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 5,
-                    name: "member 5",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 6,
-                    name: "member 6",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 7,
-                    name: "member 7",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 8,
-                    name: "member 8",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 9,
-                    name: "member 9",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 10,
-                    name: "member 10",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 11,
-                    name: "member 11",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 12,
-                    name: "member 12",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 13,
-                    name: "member 13",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 14,
-                    name: "member 14",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 15,
-                    name: "member 15",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 16,
-                    name: "member 16",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 17,
-                    name: "member 17",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 18,
-                    name: "member 18",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 19,
-                    name: "member 19",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 20,
-                    name: "member 20",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 21,
-                    name: "member 21",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 22,
-                    name: "member 22",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-                {
-                    id: 23,
-                    name: "member 23",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ornare hendrerit.",
-                    img: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
-                    githubLink: "https://github.com/explore",
-                    linkedinLink: "https://www.linkedin.com/"
-                },
-            ]
-            const temp = []
-            for (let i = total; i < total + perLoad; i++) {
-                if (!members[i]) return
-                temp.push(members[i]);
-            }
-            setTotal(total + perLoad);
-            setMembersList(membersList => [...membersList, ...temp]);
-            setIsFetching(false);
-        }, 3500)
+        // setTimeout(() => {
+        //     // temporary members data
+        //     // const members = 
+        //     const temp = []
+        //     for (let i = total; i < total + perLoad; i++) {
+        //         if (!members[i]) return
+        //         temp.push(members[i]);
+        //     }
+        //     setTotal(total + perLoad);
+        //     setMembersList(membersList => [...membersList, ...temp]);
+        //     setIsFetching(false);
+        // }, 3500)
+        setCurrentPage(currentPage + 1);
+        var res = await getMembersAsync({perPage, page: currentPage});
+        setMembersList(membersList => [...membersList, ...res]);
     }
 
+    // Init effect
     useEffect(() => {
         window.addEventListener("scroll", handleScroll, false);
         return () => {
@@ -245,7 +64,7 @@ const CardsGrid = () => {
     // hook listen to isFetching indicator to load more content
     useEffect(() => {
         if (!isFetching) return;
-        loadMore();
+        (async () => await loadMore())();
     }, [isFetching]);
 
 
