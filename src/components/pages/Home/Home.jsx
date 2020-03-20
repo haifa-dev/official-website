fc6157698741e41b27313b5167b380a248b4e718
Giora Guttsait <giora111@gmail.com>
2020-03-20 21:24:18 +0200

    Fix & character to html variant

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -58,8 +58,13 @@ const Home = () => (
         <p>
           If you'd like to provide us with a lecture, host our next meetup, or help us improve our website, please <b>contact David Klein</b> via <a href="https://www.linkedin.com/in/david-klein-835048161/">LinkedIn</a> or by <a href="mailto:davidklein.4496@gmail.com">Email</a>.
         </p>
+<<<<<<< HEAD
         <p>
           We welcome you to join us on <a href="https://chat.whatsapp.com/G26LTepUrpT2W3GS26mpoo">WhatsApp</a> &amps; <a href="https://www.facebook.com/groups/haifadev/">Facebook</a>, and follow our meetup announcements as member of our <a href="https://www.meetup.com/Haifa-dev/">Meetup group</a>.
+=======
+				<p>
+					We welcome you to join us on <a href="https://chat.whatsapp.com/G26LTepUrpT2W3GS26mpoo">WhatsApp</a> &amps; <a href="https://www.facebook.com/groups/haifadev/">Facebook</a>, and follow our meetup announcements as member of our <a href="https://www.meetup.com/Haifa-dev/">Meetup group</a>.
+>>>>>>> ae60d2d... Fix & character to html variant
         </p>
         <p>
           If you're new to development and would like to learn how to code, network with professionals and learn together, join the freeCodeCamp Haifa <a href="https://chat.whatsapp.com/8skL1KY0Nhv1uy3MDYbLIR">WhatsApp group</a> and we'll meet you as soon as our next weekly meetup!
