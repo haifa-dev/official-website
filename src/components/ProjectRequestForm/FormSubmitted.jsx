307cc83e15b99f545e5bc21251dea81f87545562
Tomer Matmon <25210484+Tomer51m@users.noreply.github.com>
2020-06-24 10:30:48 +0300

    added formSubmitted component

---
+++ src/components/ProjectRequestForm/FormSubmitted.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function FormSubmitted() {
+    return (
+        <div>
+            <h3>Form Submitted</h3>
+            <p>Your request was proccesed. Our team will get back to you ASAP.</p>
+        </div>
+    )
+}
