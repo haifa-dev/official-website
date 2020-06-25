ef6f965fd02c270ed558489b45b5f1924e972920
Tomer Matmon <25210484+Tomer51m@users.noreply.github.com>
2020-06-25 14:32:45 +0300

    replaced enchor tags with react ref

---
+++ src/components/pages/Home/About.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <>
+      <article id="about">
+        <h4>Who are we?</h4>
+        <p>
+          We're an enthusiastic group of developers from Haifa. Some of us are
+          professionals, some are still learning, but all of us agree upon one
+          thing: Haifa offers barely enough for local developers to thrive.
+        </p>
+        <p>
+          Nobody wants to move far away from the place they call home just to
+          establish a career. We don't think anyone should. That's why we've
+          established Haife:Dev, so that all of Haifa's developers will have{" "}
+          <b>a reason to stay in Haifa</b>.
+        </p>
+      </article>
+
+      <article>
+        <h4>What do we want?</h4>
+        <p>We want our careers to thrive in the city we call home.</p>
+        <p>
+          Our goal is to bring professional software-related content to Haifa{" "}
+          <b>for free</b>. We aim to organize meetups and other events that can
+          help developers in Haifa develop their career without the need to
+          leave their home, or travel even farther if the live in the north.
+        </p>
+      </article>
+
+      <article>
+        <h4>How do we do that?</h4>
+        <p>
+          We're establishing partnership with local companies and office
+          complexes as hosts for our professional meetups. We're bringing
+          professional developers and lectors from all around the country to
+          provide top quality content to our dearest Haifa developers.
+        </p>
+        <p>
+          For beginners and students, we've partnered with{" "}
+          <a href="http://www3.haifa.muni.il/inventuipub/">In-Vent</a> to
+          establish weekly study group meetups in association with the{" "}
+          <a href="https://www.freecodecamp.org/">freeCodeCamp</a> non-profit
+          organization.
+        </p>
+      </article>
+    </>
+  );
+}
