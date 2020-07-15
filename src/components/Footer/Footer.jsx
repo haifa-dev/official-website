3038ec4c7b616196592da51828f306885ef81c3f
Tomer Matmon <25210484+Tomer51m@users.noreply.github.com>
2020-07-15 13:36:55 +0300

    refactor footer with bootstrap

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 
 const Footer = () => (
-  <Navbar as="footer" bg="dark" variant="dark" className="py-0">
+  <Navbar as="footer" bg="dark" variant="dark" className="py-0 footer">
     <Navbar.Text className="text-center mx-auto">
       © 2019 haifa:dev community (non-profit). View this website's source code
-      <a href="https://github.com/haifa-dev/haifa-dev-website">here</a>.
+      <a href="https://github.com/haifa-dev/haifa-dev-website"> here</a>.
     </Navbar.Text>
   </Navbar>
 );
