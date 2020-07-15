4d0a9552c73497e0146328005f2b8a9711522afb
Tomer Matmon <25210484+Tomer51m@users.noreply.github.com>
2020-07-15 13:36:38 +0300

    added containers to website header and content

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import "./app.scss"
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Footer from '../Footer/Footer';
+import React from "react";
+import "./app.scss";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Footer from "../Footer/Footer";
 import Navigation from "../Navigation/Navigation";
-import Home from '../pages/Home/Home';
-import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
+import Home from "../pages/Home/Home";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 import RequestForm from "../pages/RequestForm/RequestsForm";
 import Projects from "../pages/Projects/Projects";
 
@@ -15,16 +15,18 @@ const Routes = () => (
     <Route exact path="/Projects" component={Projects} />
     <Route exact path="*" component={NotFoundPage} />
   </Switch>
-)
+);
 
 const App = () => (
   <BrowserRouter>
-    <div className="fixed-top bg-secondary">
-        <Navigation />
+    <div className="pageHeader">
+      <Navigation />
     </div>
-    <Routes />
-    <Footer />
+    <div className="pageContent">
+      <Routes />
+      <Footer />
+    </div>
   </BrowserRouter>
-)
+);
 
-export default App
+export default App;
