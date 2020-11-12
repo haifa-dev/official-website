63e34c937efbc2babe6a845536c284497cd95e53
cHidoriPunk <dkalaora@gmail.com>
2020-11-12 21:09:25 +0200

    Add descriptions to request form's fields
    https://github.com/haifa-dev/official-website/issues/43

--- src/components/ProjectRequestForm/FormUser.jsx
+++ src/components/ProjectRequestForm/FormUser.jsx
@@ -1,32 +1,25 @@
-import React from "react";
-import { Formik } from "formik";
-import Form from "react-bootstrap/Form";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import * as yup from "yup";
+import React from 'react';
+import { Formik } from 'formik';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import * as yup from 'yup';
+import styles from './FormStyles.module.scss';
+import FieldDescription from './FieldDescription';
 
 export default function FormMain({ formState, setFormState, loadNextForm }) {
   return (
     <Formik
       initialValues={formState}
       validationSchema={yup.object().shape({
-        name: yup
-          .string()
-          .min(2, "minimum 2 letters")
-          .required("Required field"),
-        email: yup.string().email("Invalid email").required("Required field"),
-        phone: yup
-          .number()
-          .typeError("Numbers only")
-          .required("Required field"),
-          about: yup
-          .string()
-          .required("Required field")
-          .min(30, "minimum 30 characters"),
-        businessType: yup.string().required("Required field"),
+        name: yup.string().min(2, 'minimum 2 letters').required('Required field'),
+        email: yup.string().email('Invalid email').required('Required field'),
+        phone: yup.number().typeError('Numbers only').required('Required field'),
+        about: yup.string().required('Required field').min(30, 'minimum 30 characters'),
+        businessType: yup.string().required('Required field')
       })}
-      onSubmit={(values) => {
-        setFormState((prev) => {
+      onSubmit={values => {
+        setFormState(prev => {
           return { ...prev, ...values };
         });
         loadNextForm(values.businessType);
@@ -61,7 +54,7 @@ export default function FormMain({ formState, setFormState, loadNextForm }) {
               <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
             </Form.Group>
           </Form.Row>
-          
+
           <Form.Row>
             <Form.Group as={Col} sm="5" md="4" controlId="field_3">
               <Form.Label>Phone Number:</Form.Label>
@@ -76,9 +69,9 @@ export default function FormMain({ formState, setFormState, loadNextForm }) {
               <Form.Control.Feedback type="invalid">{errors.phone}</Form.Control.Feedback>
             </Form.Group>
 
-            <Form.Group as={Col} sm="5" md="4"  controlId="field_4">
+            <Form.Group as={Col} sm="5" md="4" controlId="field_4">
               <Form.Label className="ml-1">Select your business type:</Form.Label>
-              <div className="ml-3 mt-2">
+              <div className="mt-2">
                 <Form.Check type="radio" custom inline>
                   <Form.Check.Input
                     type="radio"
@@ -86,11 +79,11 @@ export default function FormMain({ formState, setFormState, loadNextForm }) {
                     name="businessType"
                     value="nonProfit"
                     onChange={handleChange}
-                    defaultChecked={values.businessType === "nonProfit" ? true : false}
+                    defaultChecked={values.businessType === 'nonProfit' ? true : false}
                     isValid={touched.businessType && !errors.businessType}
                     isInvalid={touched.businessType && errors.businessType}
-                   />
-                   <Form.Check.Label htmlFor="nonProfit">Non-Profit</Form.Check.Label>
+                  />
+                  <Form.Check.Label htmlFor="nonProfit">Non-Profit</Form.Check.Label>
                 </Form.Check>
 
                 <Form.Check type="radio" custom inline>
@@ -100,7 +93,7 @@ export default function FormMain({ formState, setFormState, loadNextForm }) {
                     name="businessType"
                     value="forProfit"
                     onChange={handleChange}
-                    defaultChecked={values.businessType === "forProfit" ? true : false}
+                    defaultChecked={values.businessType === 'forProfit' ? true : false}
                     isValid={touched.businessType && !errors.businessType}
                     isInvalid={touched.businessType && errors.businessType}
                   />
@@ -111,8 +104,11 @@ export default function FormMain({ formState, setFormState, loadNextForm }) {
           </Form.Row>
 
           <Form.Row>
-           <Form.Group as={Col} sm="10" md="8" controlId="field_5">
-              <Form.Label>Tell us about your project:</Form.Label>
+            <Form.Group as={Col} sm="10" md="8" controlId="field_5">
+              <Form.Label>
+                <span>Tell us about your project </span>
+                <FieldDescription>Please write a minimum of 30 characters</FieldDescription>
+              </Form.Label>
               <Form.Control
                 as="textarea"
                 name="about"
@@ -121,6 +117,7 @@ export default function FormMain({ formState, setFormState, loadNextForm }) {
                 onChange={handleChange}
                 isValid={touched.about && !errors.about}
                 isInvalid={touched.about && errors.about}
+                min-length={30}
               />
               <Form.Control.Feedback type="invalid">{errors.about}</Form.Control.Feedback>
             </Form.Group>
